6df10b8094b64f32e2f362af129ef1535cc128f2
Jason Kim <jason33kim@gmail.com>
2020-01-14 20:29:12 -0500

    new task view added

---
+++ Tasks/Tasks/Views/NewTaskView.swift
@@ -0,0 +1,23 @@
+//
+//  NewTaskView.swift
+//  Tasks
+//
+//  Created by Jason Kim on 2020-01-14.
+//  Copyright © 2020 Jason Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    @State var text = ""
+    
+    var body: some View {
+        TextField("Task Name", text: $text)
+    }
+}
+
+struct NewTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTaskView()
+    }
+}
