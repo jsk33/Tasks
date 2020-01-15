5be8155b5e877c7e71cf96920c29e3d4f86bb522
Jason Kim <jason33kim@gmail.com>
2020-01-14 19:48:09 -0500

    getting started with creating a list of tasks

---
+++ Tasks/Tasks/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  Tasks
+//
+//  Created by Jason Kim on 2020-01-14.
+//  Copyright © 2020 Jason Kim. All rights reserved.
+//
+
+struct Task {
+    var name: String
+    var completed = false
+}
---
+++ Tasks/Tasks/Models/TaskStore.swift
@@ -0,0 +1,15 @@
+//
+//  TaskStore.swift
+//  Tasks
+//
+//  Created by Jason Kim on 2020-01-14.
+//  Copyright © 2020 Jason Kim. All rights reserved.
+//
+
+class TaskStore {
+    var tasks: [Task] = [Task(name: "do this"), Task(name: "do that")]
+    
+//    init(tasks: [Task]) {
+//        self.tasks = tasks
+//    }
+}
